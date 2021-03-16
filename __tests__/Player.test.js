f3fb0924a4791a532115af84b4c749eadec79caa
phil cowan <philipjcowan1@gmail.com>
2021-03-15 17:39:44 -0700

    adds inventory to the player constructor that creates two new potions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,9 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
 
 test('Creates a player object', () => {
     const player = new Player('Dave');
@@ -7,4 +12,5 @@ test('Creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]))
 });
